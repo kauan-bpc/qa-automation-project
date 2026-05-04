61370937699f02cfdd5556ae81ccd1a586c035fd
Kauã Barbosa <kauanbarbosa102938@Gmail.com>
2026-05-04 19:44:23 -0300

    fix:Eu tinha commitado com o --headless comentado sem querer

--- conftest.py
+++ conftest.py
@@ -11,7 +11,7 @@ def base_url_api():
 @pytest.fixture(scope="function")
 def driver():
     options = Options()
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
