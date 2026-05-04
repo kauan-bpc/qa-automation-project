1bf86cd4286053215d0dfdc21d28d40ef2460519
Kauã Barbosa <kauanbarbosa102938@Gmail.com>
2026-05-04 18:41:35 -0300

    fix: teste de web n passando na pipeline

--- conftest.py
+++ conftest.py
@@ -1,14 +1,13 @@
-try:
-    import pytest  # type: ignore[import]
-except ImportError:
-    pytest = None
-from selenium import webdriver 
+import pytest
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 
 @pytest.fixture(scope="session")
 def base_url_api():
     return "https://petstore.swagger.io/v2"
+
 
 @pytest.fixture(scope="function")
 def driver():
@@ -16,6 +15,9 @@ def driver():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--remote-debugging-port=9222")
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
     yield driver
